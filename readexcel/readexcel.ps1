508bcb398326c65cda1bc4da0d4930d8dca18ba6
Deo Bissessar <bissessardeo@gmail.com>
2018-05-08 21:17:23 -0400

    fixed issue with loop

--- readexcel/readexcel.ps1
+++ readexcel/readexcel.ps1
@@ -1,3 +1,7 @@
+#prep file for sendkeys
+$wshell = New-Object -ComObject wscript.shell
+$wshell.AppActivate('notepad')
+
 #declare path where excel is stored.
 $file = "C:\Users\Deus_\Desktop\PowerShell Tutorials\powershell\readexcel\readcsvtest.xlsx"
 $sheetname = "readcsvtest"
@@ -20,26 +24,29 @@ for ($i=1; $i -le $rowMax - 1; $i++) {
     $serial = $sheet.Cells.Item($rowSerial + $i,$colSerial).text
     $asset = $sheet.Cells.Item($rowAsset + $i,$colAsset).text
 
-    <#
-    $wshell = New-Object -ComObject wscript.shell
-    $wshell.AppActivate('notepad')
-    Sleep 1
-    $wshell.SendKeys($serial)
-    Sleep 1
-    $wshell.SendKeys('~')
-    Sleep 1
-    $wshell.SendKeys($asset)
-    Sleep 1
-    $wshell.SendKeys('~')
-    Sleep 1
-    $wshell.SendKeys($serial)
-    Sleep 1
-    $wshell.SendKeys('~')
-    #>
-
+    #send to external
+    Sleep 1.5
+    Add-Type -AssemblyName System.Windows.Forms
+    [System.Windows.Forms.SendKeys]::SendWait($serial)
+    Sleep 1.5
+    Add-Type -AssemblyName System.Windows.Forms
+    [System.Windows.Forms.SendKeys]::SendWait('~')
+    Sleep 1.5
+    Add-Type -AssemblyName System.Windows.Forms
+    [System.Windows.Forms.SendKeys]::SendWait($asset)
+    Sleep 1.5
+    Add-Type -AssemblyName System.Windows.Forms
+    [System.Windows.Forms.SendKeys]::SendWait('~')
+    Sleep 1.5
+    Add-Type -AssemblyName System.Windows.Forms
+    [System.Windows.Forms.SendKeys]::SendWait($serial)
+    Sleep 1.5
+    Add-Type -AssemblyName System.Windows.Forms
+    [System.Windows.Forms.SendKeys]::SendWait('~')  
+    
     #use write host to check data, eventually replace with copy and paste
-    Write-Host ("Serial Number: " + $serial)
-    Write-Host ("Asset Tag: " + $asset)
+    #Write-Host ("Serial Number: " + $serial)
+    #Write-Host ("Asset Tag: " + $asset)
 }
 
 #close excel or it will be locked for editing
